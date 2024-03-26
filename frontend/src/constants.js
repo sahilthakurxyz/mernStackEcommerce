0900531332c2dd45cee5dbd31f818ae391b7b067
sahil thakur <sahilthakurxyz@gmail.com>
2024-03-26 23:40:57 +0530

    bit change again

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // export const BACKEND_URL = "http://localhost:4001";
 // export const BACKEND_URL_PROD = "https://mernstackecommerce-1.onrender.com";
-export const BACKEND_URL_PROD = "https://mernstackecommerce-5.onrender.com/";
+export const BACKEND_URL_PROD = "https://mernstackecommerce-1.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: BACKEND_URL_PROD,
